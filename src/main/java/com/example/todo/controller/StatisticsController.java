86df9b3d32f37de49d4ca3855c389a4d4b2d2e56
Jakov992 <jakov.kusic@gmail.com>
2020-09-10 12:22:04 +0200

    Add statistics tab

--- src/main/java/com/example/todo/controller/StatisticsController.java
+++ src/main/java/com/example/todo/controller/StatisticsController.java
@@ -1,4 +1,15 @@
 package com.example.todo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class StatisticsController {
+
+    @GetMapping("/statistics")
+    public String viewStatistics(Model model) {
+
+        return "statistics/main";
+    }
 }
