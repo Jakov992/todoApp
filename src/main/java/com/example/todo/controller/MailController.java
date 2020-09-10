3e71dd22c54d01d9868eefb8ae44e191471df6eb
Jakov992 <jakov.kusic@gmail.com>
2020-09-10 16:25:06 +0200

    Add sendingMailForm template

---
+++ src/main/java/com/example/todo/controller/MailController.java
@@ -0,0 +1,15 @@
+package com.example.todo.controller;
+
+import org.json.JSONArray;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MailController {
+
+    @GetMapping("/sendingMailForm")
+    public String viewStatistics(Model model) {
+        return "sendingMail/mail_form";
+    }
+}
--- src/main/java/com/example/todo/controller/StatisticsController.java
+++ src/main/java/com/example/todo/controller/StatisticsController.java
@@ -1,16 +1,12 @@
 package com.example.todo.controller;
 
-import ch.qos.logback.core.pattern.util.IEscapeUtil;
-import com.example.todo.service.AssignmentService;
 import com.example.todo.service.StatisticsService;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Controller
 public class StatisticsController {
 
